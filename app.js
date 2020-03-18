2b760a1216e84885a455193c5c0b186022b8ad08
kinming92 <kinming.klm@gmail.com>
2020-03-17 23:57:40 -0700

    reset up the dependencies

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const MONGODB_URI =
-  //`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00-fouup.mongodb.net:27017,cluster0-shard-00-01-fouup.mongodb.net:27017,cluster0-shard-00-02-fouup.mongodb.net:27017/${process.env.MONGO_DEFAULT_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
-  `mongodb://kin:1234@cluster0-shard-00-00-fouup.mongodb.net:27017,cluster0-shard-00-01-fouup.mongodb.net:27017,cluster0-shard-00-02-fouup.mongodb.net:27017/movies?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00-fouup.mongodb.net:27017,cluster0-shard-00-01-fouup.mongodb.net:27017,cluster0-shard-00-02-fouup.mongodb.net:27017/${process.env.MONGO_DEFAULT_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
